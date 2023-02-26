60956d4668bda5ea658df6aa05c6b7a0d81de508
Mischa <mikeyaremenko@gmail.com>
2023-02-26 16:31:05 +0100

    feat: initial project structure

---
+++ src/main/kotlin/kharkov/card/games/deberc/DebercApplication.kt
@@ -0,0 +1,11 @@
+package kharkov.card.games.deberc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DebercApplication
+
+fun main(args: Array<String>) {
+	runApplication<DebercApplication>(*args)
+}
---
+++ src/test/kotlin/kharkov/card/games/deberc/DebercApplicationTests.kt
@@ -0,0 +1,13 @@
+package kharkov.card.games.deberc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DebercApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
