6fe66722d57de014133bf1cdffa0c9a47e7e29a2
Mischa <mikeyaremenko@gmail.com>
2023-03-05 11:18:51 +0100

    chore: configure flyway & fix migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,10 @@ plugins {
 	kotlin("plugin.spring") version "1.7.22"
 }
 
+ext {
+	set("testcontainersVersion", "1.17.6")
+}
+
 group = "kharkov.card.games"
 version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -25,12 +29,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.jooq:jooq:3.14.15")
+
+	implementation("org.jooq:jooq:3.17.8")
 
 	jdbc("org.postgresql:postgresql:42.2.5")
 
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:r2dbc-postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
@@ -42,19 +48,38 @@ tasks.withType<KotlinCompile> {
 	}
 }
 
+jooq {
+	version = "3.12.2"
+	jdbc {
+		db = "jdbc:postgresql://host.docker.internal:5432/deberc"
+		username = "deberc"
+		password = "deberc"
+	}
+}
+
 tasks {
 	generateJooqClasses {
 		schemas = arrayOf("public", "other_schema")
 		basePackageName = "org.jooq.generated"
 		inputDirectory.setFrom(project.files("src/main/resources/db/migration"))
-		outputDirectory.set(project.layout.buildDirectory.dir("generated-jooq"))
+		outputDirectory.set(project.layout.buildDirectory.dir("generated/jooq"))
 		flywayProperties = mapOf("flyway.placeholderReplacement" to "false")
 		excludeFlywayTable = true
 		outputSchemaToDefault = setOf("public")
 		schemaToPackageMapping = mapOf("public" to "kharkov.deberc")
+		dependsOn("flywayMigrate")
 		customizeGenerator {
 			/* "this" here is the org.jooq.meta.jaxb.Generator configure it as you please */
 		}
+	}
+
+	flywayMigrate {
+		driver = "org.postgresql.Driver"
+		url = "jdbc:postgresql://localhost:5432/deberc"
+		user = "deberc"
+		password = "deberc"
+		outputs.dir("${project.buildDir}/generated/flyway")
+		doFirst { delete(outputs.files) }
 	}
 }
 
